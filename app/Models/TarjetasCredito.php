766235a03c50dc3128707325343ac91f75b6d6ac
Anahí Pérez Ponce <anahiperezpkmn@gmail.com>
2021-02-16 00:38:46 +0000

    v7

--- app/Models/TarjetasCredito.php
+++ app/Models/TarjetasCredito.php
@@ -1,11 +1,15 @@
 <?php
-
-
 namespace Models;
+require 'app/Models/Conexion.php';
+use Models\Conexion;
 
-
-class TarjetasCredito
+class TarjetasCredito extends Conexion
 {
+    public $folio_Tarjeta;
+    public $fechVencimiento;
+    public $noSeguridad;
+    public $compania;
+    public $id_Usuario;
 
     function __construct()
     {

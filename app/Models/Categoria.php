43aacf800946fcbe331f3ce020fbf1da208ad3cc
Anahí Pérez Ponce <anahiperezpkmn@gmail.com>
2021-02-16 00:25:00 +0000

    v7

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -1,11 +1,11 @@
 <?php
-
-
 namespace Models;
+require 'app/Models/Conexion.php';
+use Models\Conexion;
 
-
-class Categoria
+class Categoria extends Conexion
 {
+    public $Nombre;
     function __construct()
     {
         parent::__construct();

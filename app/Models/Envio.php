493b1678097cfc5493a583f9a05f905d092fc963
Anahí Pérez Ponce <anahiperezpkmn@gmail.com>
2021-02-16 00:30:27 +0000

    v7

--- app/Models/Envio.php
+++ app/Models/Envio.php
@@ -1,11 +1,14 @@
 <?php
-
-
 namespace Models;
+require 'app/Models/Conexion.php';
+use Models\Conexion;
 
-
-class Envio
+class Envio extends Conexion
 {
+    
+    public $id_Producto;
+    public $estatus;
+    
     function __construct()
     {
         parent::__construct();

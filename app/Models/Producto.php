92eeaa2bfc6cd2b17da1d2f35759b6bae344f5f5
Anahí Pérez Ponce <anahiperezpkmn@gmail.com>
2021-02-16 00:35:46 +0000

    v7

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -1,11 +1,21 @@
 <?php
-
-
+require 'app/Models/Conexion.php';
+use Models\Conexion;
 namespace Models;
 
 
-class Producto
+class Producto extends Conexion
 {
+    public $id;
+    public $nombre;
+    public $categoria;
+    public $tipo;
+    public $artista;
+    public $precio
+    public $stock;
+    public $anio;
+    public $proveedor;
+    public $imagen;
     function __construct()
     {
         parent::__construct();
